873ba92013f5c33622ad39a3949a4ce800c339ec
emata10 <130502916+emata10@users.noreply.github.com>
2023-07-07 12:42:16 -0600

    HelloWorld

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,28 @@
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         String world = "World";
         System.out.println(StringUtils.prependIfMissing(world, "Hello, "));
+
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter Something: ");
+            String input = scanner.nextLine();
+            System.out.println("You Entered: \"" + input + "\"");
+
+            if (StringUtils.isNumeric(input)) {
+                System.out.println("\"" + input + "\" is a number");
+            } else {
+                System.out.println("\"" + input + "\" is not a number");
+            }
+
+            String flippedCase = StringUtils.swapCase(input);
+            System.out.println("Flipped Case: " + flippedCase);
+
+            String reversedString = StringUtils.reverse(input);
+            System.out.println("Reversed: " + reversedString);
     }
 }
